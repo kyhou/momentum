d80003e4d6e52224c76a4c71944f00a020daeafd
kyhou <joelskill@gmail.com>
2023-12-13 00:15:23 -0300

    Alterado formatação dos textos dos menu

--- src/components/MenuItems.vue
+++ src/components/MenuItems.vue
@@ -2,32 +2,32 @@
     <template v-if="showUserContent">
         <div class="divider m-0 opacity-100 sm:divider-horizontal"></div>
         <li class="menu-title lg:hidden">
-            <span>usuário</span>
+            <span>Usuário</span>
         </li>
         <li>
-            <router-link to="/dashboard" class="link link-hover uppercase text-menu-text">meus investimentos</router-link>
+            <router-link to="/dashboard" class="link link-hover text-menu-text">Meus Investimentos</router-link>
         </li>
     </template>
     <template v-if="showColaboratorContent">
         <div class="divider m-0 text-menu-text opacity-100 sm:divider-horizontal"></div>
         <li class="menu-title lg:hidden">
-            <span>colaborador</span>
+            <span>Colaborador</span>
         </li>
     </template>
     <template v-if="showAdminContent">
         <div class="divider m-0 text-menu-text opacity-100 sm:divider-horizontal"></div>
         <li class="menu-title lg:hidden">
-            <span>admin</span>
+            <span>Admin</span>
         </li>
         <li class="lg:hidden">
-            <router-link to="/admin/users" class="link link-hover text-menu-text">usuários</router-link>
-            <router-link to="/admin/aportes" class="link link-hover text-menu-text">aportes</router-link>
-            <router-link to="/admin/transactions" class="link link-hover text-menu-text">transações</router-link>
-            <router-link to="/admin/withdraws" class="link link-hover text-menu-text">saques</router-link>
-            <router-link to="/admin/profits" class="link link-hover text-menu-text">vigências</router-link>
+            <router-link to="/admin/users" class="link link-hover text-menu-text">Usuários</router-link>
+            <router-link to="/admin/aportes" class="link link-hover text-menu-text">Aportes</router-link>
+            <router-link to="/admin/transactions" class="link link-hover text-menu-text">Transações</router-link>
+            <router-link to="/admin/withdraws" class="link link-hover text-menu-text">Saques</router-link>
+            <router-link to="/admin/profits" class="link link-hover text-menu-text">Vigências</router-link>
         </li>
         <li class="max-lg:hidden" tabindex="0">
-            <a class="uppercase text-menu-text">
+            <a class="text-menu-text">
                 Admin
                 <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
                     <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
@@ -35,19 +35,19 @@
             </a>
             <ul class="bg-primary p-2">
                 <li>
-                    <router-link to="/admin/users" class="link link-hover text-menu-text">usuários</router-link>
+                    <router-link to="/admin/users" class="link link-hover text-menu-text">Usuários</router-link>
                 </li>
                 <li>
-                    <router-link to="/admin/aportes" class="link link-hover text-menu-text">aportes</router-link>
+                    <router-link to="/admin/aportes" class="link link-hover text-menu-text">Aportes</router-link>
                 </li>
                 <li>
-                    <router-link to="/admin/transactions" class="link link-hover text-menu-text">transações</router-link>
+                    <router-link to="/admin/transactions" class="link link-hover text-menu-text">Transações</router-link>
                 </li>
                 <li>
-                    <router-link to="/admin/withdraws" class="link link-hover text-menu-text">saques</router-link>
+                    <router-link to="/admin/withdraws" class="link link-hover text-menu-text">Saques</router-link>
                 </li>
                 <li>
-                    <router-link to="/admin/profits" class="link link-hover text-menu-text">vigências</router-link>
+                    <router-link to="/admin/profits" class="link link-hover text-menu-text">Vigências</router-link>
                 </li>
             </ul>
         </li>
