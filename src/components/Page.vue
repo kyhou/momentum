060aa91ed012520b37da6dd95dda1f7bfb956007
kyhou <joelskill@gmail.com>
2024-01-01 02:04:47 -0300

    Alterado textos.

--- src/components/Page.vue
+++ src/components/Page.vue
@@ -16,33 +16,33 @@
                             aria-label="Site Nav" class="block">
                             <li>
                                 <a class="link link-hover text-white transition" href="#about">
-                                    sobre nós
+                                    Sobre nós
                                 </a>
                             </li>
                             <li>
                                 <a class="link link-hover text-white transition" href="#properties">
-                                    imóveis
+                                    Imóveis
                                 </a>
                             </li>
                             <li>
                                 <a class="link link-hover text-white transition" href="#contact">
-                                    contato
+                                    Contato
                                 </a>
                             </li>
                             <div class="divider m-0"></div>
-                            <li class="menu-title">
-                                <span>corporativo</span>
+                            <li class="menu-title text-neutral">
+                                <span>Corporativo</span>
                             </li>
-                            <li><router-link to="/policies" class="link link-hover text-white">políticas</router-link>
+                            <li><router-link to="/policies" class="link link-hover text-white">Políticas</router-link>
                             </li>
                             <li><router-link to="/institutional"
-                                    class="link link-hover text-white">institucional</router-link></li>
+                                    class="link link-hover text-white">Institucional</router-link></li>
                         </nav>
                         <MenuItems />
                     </ul>
                 </div>
                 <a class="w-45">
-                    <span class="sr-only">Momentum</span>
+                    <span class="sr-only">Momentum Invest</span>
                     <a href="/" class="btn btn-ghost normal-case">
                         <template v-if="this.user == null ||
                             this.user.type == 'select' ||
@@ -66,35 +66,35 @@
                             v-if="!((this.$route.fullPath == '/policies') || (this.$route.fullPath == '/institutional'))">
                             <li>
                                 <a class="link link-hover text-white transition" href="#about">
-                                    sobre nós
+                                    Sobre nós
                                 </a>
                             </li>
                             <li>
                                 <a class="link link-hover text-white transition" href="#properties">
-                                    imóveis
+                                    Imóveis
                                 </a>
                             </li>
                             <li>
                                 <a class="link link-hover text-white transition" href="#contact">
-                                    contato
+                                    Contato
                                 </a>
                             </li>
                             <div class="dropdown dropdown-bottom">
-                                <a tabindex="0" class="link link-hover text-white">corporativo</a>
+                                <a tabindex="0" class="link link-hover text-white">Corporativo</a>
                                 <ul tabindex="0" class="dropdown-content menu rounded-box w-52 bg-primary p-2 shadow">
                                     <li><router-link to="/policies"
-                                            class="link link-hover text-white">políticas</router-link></li>
+                                            class="link link-hover text-white">Políticas</router-link></li>
                                     <li><router-link to="/institutional"
-                                            class="link link-hover text-white">institucional</router-link></li>
+                                            class="link link-hover text-white">Institucional</router-link></li>
                                 </ul>
                             </div>
                         </template>
                         <template v-else>
                             <li><router-link to="/policies"
-                                    class="link link-hover text-white transition">políticas</router-link>
+                                    class="link link-hover text-white transition">Políticas</router-link>
                             </li>
                             <li><router-link to="/institutional"
-                                    class="link link-hover text-white transition">institucional</router-link></li>
+                                    class="link link-hover text-white transition">Institucional</router-link></li>
                         </template>
                     </ul>
                 </nav>
@@ -103,8 +103,8 @@
                 <ul class="menu menu-horizontal hidden px-1 font-semibold lg:mr-3 lg:flex">
                     <MenuItems />
                 </ul>
-                <a v-if="!currentUser" class="btn-neutral btn font-bold" @click.prevent="logIn">entrar</a>
-                <a v-else class="btn-neutral btn font-bold" @click.prevent="logOut">sair</a>
+                <a v-if="!currentUser" class="btn-neutral btn font-bold" @click.prevent="logIn">Entrar</a>
+                <a v-else class="btn-neutral btn font-bold" @click.prevent="logOut">Sair</a>
             </div>
         </div>
     </header>
@@ -147,15 +147,7 @@ export default {
         window.addEventListener('resize', this.handleResize);
         this.handleResize();
 
-        if (this.$route.name != 'home' &&
-            this.$route.fullPath != '/policies' &&
-            this.$route.fullPath != '/institutional' &&
-            this.user != null &&
-            this.user.type != null) {
-            document.documentElement.setAttribute('data-theme', this.user.type);
-        } else {
-            document.documentElement.setAttribute('data-theme', 'select');
-        }
+        document.documentElement.setAttribute('data-theme', 'select');
     },
     mounted() {
         const user = JSON.parse(localStorage.getItem('user'));
