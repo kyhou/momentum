a83383aea3f2dda86a7e922a33ab4d65ee94767b
kyhou <joelskill@gmail.com>
2024-08-04 22:25:47 -0300

    Revert "Melhora na geração da baseURL."
    This reverts commit ee2ce00b1f68a273a5b3208b7c87965c8dd8ef6e.

--- src/common/http-common.js
+++ src/common/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: (import.meta.env.VITE_API_ADDR || (('http://localhost:' + (import.meta.env.VITE_PORT || 8088)) + '/api')),
+    baseURL: (import.meta.env.VITE_API_ADDR || ('http://localhost:' + (import.meta.env.VITE_PORT || 8088))) + '/api',
     headers: {
         "Content-type": "application/json"
     }
